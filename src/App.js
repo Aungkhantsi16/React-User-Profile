e581f9436d62462fc9a6c9a3c4478a57f9e65b34
Aung Khant Si <mikeisgreat02@gmail.com>
2024-01-12 20:37:50 +0630

    Users Profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  async function getUsers() {
+    const response = await fetch(url);
+    const users = await response.json();
+
+    if (response.status > 300) {
+      setIsError(true);
+      setIsLoading(false);
+    }
+
+    setUsers(users);
+    setIsLoading(false);
+    console.log(users);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  console.log("component is rendered");
+  if (isLoading) {
+    return <h1>Loading......</h1>;
+  }
+  if (isError) {
+    return <h1>Opps Error...</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>GitHub Users</h1>
+      <ul className="users">
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <img src={user.avatar_url} alt={user.login} />
+              <h4>{user.login}</h4>
+              <a href={user.html_url}>Profile</a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
